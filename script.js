12d8219c3bf426004cd33d7d7198ecf9a6828f02
ralfsreinsons <ralfsreinsons64@gmail.com>
2026-09-16 20:47:28 +0300

    Added pictures to js script

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const button = document.getElementById("myButton");
 button.addEventListener("click", function() {
     window.location.href = "pictures.html";
 });
+
+const pictures = [
+    "pictures/jsFunct.png",
+    "pictures/gitrepo.png",
+
+];
+
+const picture = document.getElementById("picture");
+const shuffleButton = document.getElementById("shuffleButton");
+
+shuffleButton.addEventListener("click", function() {
+    const randomIndex = Math.floor(Math.random() * pictures.length);
+    picture.src = pictures[randomIndex];
+});
