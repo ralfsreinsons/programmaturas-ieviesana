414e1c803158cda7395490c43717dc542b5103f5
ralfsreinsons <ralfsreinsons64@gmail.com>
2026-09-16 20:52:37 +0300

    Fixed- pictures page shuffle button now selects a random image

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const button = document.getElementById("myButton");
 
-button.addEventListener("click", function() {
-    window.location.href = "pictures.html";
-});
+if (button) {
+    button.addEventListener("click", function() {
+        window.location.href = "pictures.html";
+    });
+}
+
 
 const pictures = [
     "pictures/jsFunct.png",
-    "pictures/gitrepo.png",
-
+    "pictures/gitrepo.png"
 ];
 
 const picture = document.getElementById("picture");
 const shuffleButton = document.getElementById("shuffleButton");
 
-shuffleButton.addEventListener("click", function() {
-    const randomIndex = Math.floor(Math.random() * pictures.length);
-    picture.src = pictures[randomIndex];
-});
+if (shuffleButton && picture) {
+    shuffleButton.addEventListener("click", function() {
+        const randomIndex = Math.floor(Math.random() * pictures.length);
+        picture.src = pictures[randomIndex];
+    });
+}
